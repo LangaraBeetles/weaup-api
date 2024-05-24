97483fd68ae3389dffcfe06ecb25d4fe3e97ee5c
Wonnnyo Hamester <wonnyohamester@outlook.com>
2024-05-23 17:35:10 -0700

    feedback

--- db.mjs
+++ db.mjs
@@ -1,16 +1,37 @@
-import { MongoClient } from 'mongodb';
+import { MongoClient } from "mongodb";
 
-const uri = process.env.DB_CONNECTION;
+const connect = async (onSuccess, onError) => {
+  try {
+    const uri = process.env.DB_CONNECTION;
 
-let client;
-let clientPromise;
+    let client;
+    let clientPromise;
 
-if (!global._mongoClientPromise) {
-    client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
-    clientPromise = client.connect();
-    global._mongoClientPromise = clientPromise;
-} else {
-    clientPromise = global._mongoClientPromise;
-}
+    if (!global._mongoClientPromise) {
+      client = new MongoClient(uri, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+      });
+      clientPromise = await client.connect();
+      global._mongoClientPromise = clientPromise;
+    } else {
+      clientPromise = global._mongoClientPromise;
+    }
 
-export default clientPromise; 
+    return onSuccess?.(clientPromise);
+  } catch (error) {
+    console.error("Database query failed", error);
+    const errorCallback = onError?.(error);
+
+    if (errorCallback) {
+      return errorCallback;
+    }
+
+    return {
+      statusCode: 500,
+      body: JSON.stringify({ error }),
+    };
+  }
+};
+
+export default connect;
--- functions/mongo-test.mjs
+++ functions/mongo-test.mjs
@@ -1,30 +1,24 @@
-import clientPromise from "../db.mjs";
+import connect from "../db.mjs";
 
 export default async (req, context) => {
+  await connect(
+    async (client) => {
+      const database = client.db("test");
+      const collection = database.collection("beetlesTest");
 
-    try {
-        
-        const client = await clientPromise;
+      const documents = await collection.find().toArray();
 
-        const database = client.db('test');
-        const collection = database.collection('beetlesTest');
-
-        const documents = await collection.find().toArray();
-
-        if (documents.length === 0) {
-            return {
-                statusCode: 404,
-                body: JSON.stringify({ error: 'No documents found.' }),
-            };
-        }
-
-        return new Response(JSON.stringify(documents));
-        
-    } catch (error) {
-        console.error('Database query failed', error);
+      if (documents.length === 0) {
         return {
-            statusCode: 500,
-            body: JSON.stringify({ error: 'Database query failed' }),
+          statusCode: 404,
+          body: JSON.stringify({ error: "No documents found." }),
         };
+      }
+
+      return new Response(JSON.stringify(documents));
+    },
+    () => {
+      console.error("Database query failed", error);
     }
+  );
 };
