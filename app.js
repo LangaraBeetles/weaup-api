ea296a5ebf6837e4fcda2a9a85a194c49e07fd61
Wonnnyo Hamester <wonnyohamester@outlook.com>
2024-05-28 16:09:10 -0700

    feat: secure mongo db connection

--- app.js
+++ app.js
@@ -1,18 +1,27 @@
 const express = require('express');
 const app = express();
 
-const connection = require("./db/connection.js");
+try {
+  const connection = require("./db/connection.js");
 
-connection.once("open", () => {
-    const server = app.listen(process.env.PORT || 8080, () => {
-      console.log("Connected and listening");
+  connection.once("open", () => {
+    const server = app.listen(process.env.PORT || 3000, () => {
+      console.log(
+        `Connected and listening http://localhost:${process.env.PORT || 3000}`
+      );
+      console.log(
+        `Test endpoint http://localhost:${process.env.PORT || 3000}/api/v1/test`
+      );
     });
   });
+} catch (error) {
+  console.error({ error });
+}
 
 //   Test the API, only plain text.
-  app.use('/api/v1/test', (req, res) => {
-    res.type('text/plain');
-    res.status(200);
-    res.send('Hello World');
-  });
+app.get("/api/v1/test", (req, res) => {
+  res.type("text/plain");
+  res.status(200);
+  res.send("Working!");
+});
 //   End test API
