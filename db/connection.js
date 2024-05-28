d0d39126b58f1c67497a9f687129f6b0e68c008f
Wonnnyo Hamester <wonnyohamester@outlook.com>
2024-05-28 16:15:33 -0700

    chore: remove mongodb warning

--- db/connection.js
+++ db/connection.js
@@ -3,6 +3,6 @@ require('dotenv').config();
 const mongoose = require("mongoose");
 
 let mongoDB = process.env.DB_CONNECTION;
-mongoose.connect(mongoDB, {useNewUrlParser: true, useUnifiedTopology: true});
+mongoose.connect(mongoDB);
 
 module.exports = mongoose.connection;
