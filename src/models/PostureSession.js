e7abe9f4865cbda2f8a438883d56d9162a5d5c33
Wonnnyo Hamester <wonnyohamester@outlook.com>
2024-06-10 09:20:30 -0700

    feat: posture session endpoints

---
+++ src/models/PostureSession.js
@@ -0,0 +1,40 @@
+import { Schema, model } from "mongoose";
+import dayjs from "dayjs";
+
+const schema = new Schema(
+  {
+    user_id: { type: String, required: true },
+    started_at: { type: Date, required: true },
+    ended_at: { type: Date, required: false },
+    duration: {
+      type: Number,
+      default: function () {
+        const startedAt = dayjs(this.started_at);
+        const endedAt = dayjs(this.ended_at);
+        if (startedAt.isValid() && endedAt.isValid()) {
+          return endedAt.diff(startedAt, "minutes");
+        }
+        return 0;
+      },
+    },
+    total_good: {
+      type: Number,
+      required: true,
+      default: 0,
+    },
+    total_bad: {
+      type: Number,
+      required: true,
+      default: 0,
+    },
+    total_records: {
+      type: Number,
+      default: function () {
+        return Number(this.total_bad) + Number(this.total_good);
+      },
+    },
+  },
+  { timestamps: true },
+);
+
+export default model("PostureSession", schema);
