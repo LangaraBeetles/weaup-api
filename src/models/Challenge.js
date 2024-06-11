5f31369b91b139f62ac3cd9bcd48dc1348f72386
Reinhardt Botha <botha.wr@gmail.com>
2024-06-10 19:39:04 -0700

    chore: update model

--- src/models/Challenge.js
+++ src/models/Challenge.js
@@ -4,9 +4,15 @@ const schema = new Schema({
   creator_id: { type: String, required: true },
   name: { type: String, required: true },
   description: { type: String, required: true },
-  start_at: { type: String, required: true },
-  end_at: { type: String, required: true },
+  start_at: { type: Date, default: Date.now(), required: false },
+  end_at: { type: Date, required: true },
   goal: { type: Number, required: true },
+  status: {
+    type: String,
+    enum: ["in_progress", "not_achieved", "achieved", "deleted"],
+    default: "in_progress",
+    required: false,
+  },
 });
 
 export default model("Challenge", schema);
