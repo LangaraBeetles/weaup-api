d7a9f4d7be097490aba641b6be4815757eff0fd2
Bethleen S. Baral <developer.beck@gmail.com>
2024-06-12 11:51:57 -0700

    GET user (#12)
    * feat: get user by id
    
    * feat: get users
    
    * chore: Update comment to match the path

--- src/controllers/user.controllers.js
+++ src/controllers/user.controllers.js
@@ -43,7 +43,36 @@ const updateUser = async (req, res) => {
     res.status(500).json({ data: null, error: JSON.stringify(error) });
   }
 };
+
+//Get User by id
+const getUserById = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const response = await User.findById(id);
+
+    if (!response) {
+      return res.status(404).json({ data: null, error: "User not found" });
+    }
+    res.status(200).json({ data: response, error: null });
+  } catch (error) {
+    res.status(500).json({ data: null, error: JSON.stringify(error) });
+  }
+};
+
+//Get Users
+const getUsers = async (req, res) => {
+  try {
+    const response = await User.find();
+
+    res.status(200).json({ data: response, error: null });
+  } catch (error) {
+    res.status(500).json({ data: null, error: JSON.stringify(error) });
+  }
+};
+
 export default {
   createUser,
   updateUser,
+  getUserById,
+  getUsers,
 };
