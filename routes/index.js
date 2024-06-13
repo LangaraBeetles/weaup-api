5b4b460ae451000164baa72582aacf3046fa7da2
Wonnnyo Hamester <wonnyohamester@outlook.com>
2024-06-13 07:43:06 -0700

    fix: delete unused file

--- routes/index.js
+++
@@ -1,8 +0,0 @@
-import express from "express";
-import challengeRoutes from "../src/routes/challenge.routes";
-
-const router = express.Router({ mergeParams: true });
-
-router.use(challengeRoutes);
-
-export default router;
