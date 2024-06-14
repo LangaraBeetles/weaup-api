0134870242f7df03f1ebb887dbfad9f9b4802c83
Victorportus <41453306+Victorportus@users.noreply.github.com>
2024-06-13 18:55:51 -0700

    fix: error on squash

--- src/constants/paths.const.js
+++ src/constants/paths.const.js
@@ -3,15 +3,11 @@ const paths = {
     records: "/posture/records",
     sessions: "/posture/sessions",
   },
-<<<<<<< CU-86b0ufcxn_GET-notifications
-  notifications: {
-    base: "/notifications",
-  },
-=======
   daily_summary: "/daily_summary",
   challenge: "/challenges",
   user: "/user",
->>>>>>> main
+  notifications: {
+    base: "/notifications",
 };
 
 export default paths;
