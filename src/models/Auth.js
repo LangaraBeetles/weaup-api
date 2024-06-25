4bfadc7308b2c286727285c9fca5c2958b42e994
Wonnnyo Hamester <wonnyohamester@outlook.com>
2024-06-25 14:50:43 -0700

    fix: build error

--- src/models/Auth.js
+++ src/models/Auth.js
@@ -2,15 +2,15 @@
 
 class AuthData {
   constructor(req) {
-    this._id = req.auth._id;
-    this.name = req.auth.name;
-    this.email = req.auth.email;
-    this.preferred_mode = req.auth.preferred_mode;
-    this.daily_goal = req.auth.daily_goal;
-    this.is_setup_complete = req.auth.is_setup_complete;
-    this.xp = req.auth.xp;
-    this.hp = req.auth.hp;
-    this.device_id = req.auth.device_id;
+    this._id = req.auth?._id;
+    this.name = req.auth?.name;
+    this.email = req.auth?.email;
+    this.preferred_mode = req.auth?.preferred_mode;
+    this.daily_goal = req.auth?.daily_goal;
+    this.is_setup_complete = req.auth?.is_setup_complete;
+    this.xp = req.auth?.xp;
+    this.hp = req.auth?.hp;
+    this.device_id = req.auth?.device_id;
   }
 }
 
